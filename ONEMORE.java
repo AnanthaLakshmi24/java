08ba5b908a5e864994bc1d8baa74857e31095009
G.Durga Anantha Lakshmi <166210110+AnanthaLakshmi24@users.noreply.github.com>
2025-06-21 22:00:32 +0530

    Add files via upload

---
+++ ONEMORE.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner in=new Scanner(System.in);
+		int T=in.nextInt();
+		for(int i=1;i<=T;i++)
+		{
+		    int x=in.nextInt();
+		    if(x>24)
+		    System.out.println("YES");
+		    else
+		    System.out.println("NO");
+		}
+
+	}
+}
