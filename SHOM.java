a149bae48debc4ec37bd3d020837bf4922bd7811
G.Durga Anantha Lakshmi <166210110+AnanthaLakshmi24@users.noreply.github.com>
2025-06-29 21:32:56 +0530

    Add files via upload

---
+++ SHOM.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+		int L=sc.nextInt();
+		int R=sc.nextInt();
+		int N=Math.abs(L-R);
+		System.out.printf("%d",N);
+		
+
+	}
+}
