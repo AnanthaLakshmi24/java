dcafec2b77323d62265908757af1d42ec677fba3
G.Durga Anantha Lakshmi <166210110+AnanthaLakshmi24@users.noreply.github.com>
2025-07-11 20:50:51 +0530

    Add files via upload

---
+++ DIVIDE3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		if(t%3==0) System.out.println("0");
+		else System.out.println("1");
+
+	}
+}
