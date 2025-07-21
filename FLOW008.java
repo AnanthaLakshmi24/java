bf2b0f6395ff0d4b19ae3a446911752880a4719d
G.Durga Anantha Lakshmi <166210110+AnanthaLakshmi24@users.noreply.github.com>
2025-07-21 19:57:11 +0530

    Add files via upload

---
+++ FLOW008.java
@@ -0,0 +1,27 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int t;
+		Scanner sc=new Scanner(System.in);
+		t=sc.nextInt();
+		while(t-->0){
+		    int N;
+		    N=sc.nextInt();
+		    if(N<10){
+	       System.out.println( "Thanks for helping Chef!");
+	    }
+	    else{
+	        System.out.println("-1");
+	    }
+		    
+		}
+	}
+}
