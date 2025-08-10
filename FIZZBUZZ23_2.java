93c67491551a4d1f492e75bb3c345a7b78db768e
G.Durga Anantha Lakshmi <166210110+AnanthaLakshmi24@users.noreply.github.com>
2025-08-10 19:34:04 +0530

    Add files via upload

---
+++ FIZZBUZZ23_2.java
@@ -0,0 +1,28 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int t=s.nextInt();
+		while(t-->0)
+		{
+		    int a,b,c;
+		    a=s.nextInt();
+		    b=s.nextInt();
+		    c=s.nextInt();
+		    System.out.println(time(a,b,c));
+		}
+	}
+	public static int time(int a,int b,int c)
+	{
+	    return((a/(b*5)+c));
+	}
+}
