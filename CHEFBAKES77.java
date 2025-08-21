6b3de9472cc3f73ffb55ef43c874539fba341f90
G.Durga Anantha Lakshmi <166210110+AnanthaLakshmi24@users.noreply.github.com>
2025-08-21 21:18:40 +0530

    Add files via upload

---
+++ CHEFBAKES77.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int X = sc.nextInt();
+		int Y = sc.nextInt();
+		int cakesPerVehicle = Y / X;  
+        int vehicles = (N + cakesPerVehicle - 1) / cakesPerVehicle; 
+		System.out.println(vehicles);
+
+	}
+}
