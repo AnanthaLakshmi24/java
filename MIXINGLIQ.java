aae2c29465f59f0b998997210a23d23063d2b16d
G.Durga Anantha Lakshmi <166210110+AnanthaLakshmi24@users.noreply.github.com>
2025-08-29 19:40:31 +0530

    Add files via upload

---
+++ MIXINGLIQ.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		
+		while(t-->0){
+		    int a = in.nextInt();
+		    int b = in.nextInt();
+		    
+		    int x = Math.min(a,b/2);
+		    int j= 3 * x; 
+		    System.out.println(j);
+
+	}
+	}
+}
