49583ae61595ff6eba4d62123fefbf0a97e4c10a
G.Durga Anantha Lakshmi <166210110+AnanthaLakshmi24@users.noreply.github.com>
2025-09-22 21:26:08 +0530

    Add files via upload

---
+++ CGYM.java
@@ -0,0 +1,26 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t, x, y, z;
+		
+		t = sc.nextInt();
+		while (t --> 0){
+		    x = sc.nextInt();
+		    y = sc.nextInt();
+		    z = sc.nextInt();
+		    if (x > z) System.out.println(0);
+		    else if (z >= x + y) System.out.println(2);
+		    else System.out.println(1);
+		}
+	}
+}
