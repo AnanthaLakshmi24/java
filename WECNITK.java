c70f3106a7960a125b580a59f0777a4d8fdb595e
G.Durga Anantha Lakshmi <166210110+AnanthaLakshmi24@users.noreply.github.com>
2025-09-26 20:22:08 +0530

    Add files via upload

---
+++ WECNITK.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        if( s.equals("WECNITK")){
+            System.out.println("Welcome to Web Club!");
+        }
+        else{
+            System.out.println("Access Denied");
+        }
+	}
+}
